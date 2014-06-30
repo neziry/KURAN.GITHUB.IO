252b1aaa26b9b79e2720918a96e217ff42c35e92
Berat Bilgin <berat@bilgin.com>
2014-06-30 20:09:31 +0300

    Çevirilerin listelenmesi.

---
+++ app.main/controllers/homeController.js
@@ -0,0 +1,10 @@
+﻿function homeController($scope, $http) {
+
+    $http.get("http://quranservice.azurewebsites.net/api/Translators")
+    .success(function (data, status, headers, config) {
+        $scope.Translators = data;
+    }).error(function (data, status, headers, config) {
+        alert(status);
+    });
+
+}
