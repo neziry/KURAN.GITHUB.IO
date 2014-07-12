29d5168c95f462f330205092c79b655526eaa3cb
Berat Bilgin <berat@bilgin.com>
2014-07-12 04:15:55 +0300

    structure edit

--- app.main/app.js
+++ app.main/app.js
@@ -1,23 +1,5 @@
 (function () {
     var app = angular.module('homeModule', []);
-
-    app.directive('booksPanel', function () {
-        return {
-            restrict: 'E',
-            templateUrl: '/app.main/views/shared/partials/pBooks.html',
-            controller: ['$http', function ($http) {
-                var books = this;
-                books.Translators = new Array();
-
-                $http.get('http://quranservice.azurewebsites.net/api/Translators')
-                .success(function (data, status, headers, config) {
-                    books.Translators = data;
-                }).error(function (data, status, headers, config) {
-                    alert(status);
-                });
-            }],
-            controllerAs: 'Books'
-        };
-    });
-
+    
+    app.directive('booksPanel', directives.BooksPanel);
 })();
---
+++ app.main/helpers/directives/mainDirectives.js
@@ -0,0 +1,25 @@
+﻿var directiveBooksPanel = function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/app.main/views/shared/partials/pBooks.html',
+        controller: ['$http', function ($http) {
+            var books = this;
+            books.Translators = new Array();
+
+            $http.get('http://quranservice.azurewebsites.net/api/Translators')
+            .success(function (data, status, headers, config) {
+                books.Translators = data;
+            }).error(function (data, status, headers, config) {
+                alert(status);
+            });
+        }],
+        controllerAs: 'Books'
+    };
+};
+
+var directives = {
+
+    BooksPanel: directiveBooksPanel,
+
+
+};
