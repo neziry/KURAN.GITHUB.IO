7de485aba78c6730a9b559881ccf3a3265426ceb
Berat Bilgin <berat@bilgin.com>
2014-07-12 11:54:31 +0300

    ngResource

--- app.main/app.js
+++ app.main/app.js
@@ -1,5 +1,5 @@
 (function () {
-    var app = angular.module('homeModule', []);
+    var app = angular.module('homeModule', ['apiServiceModule']);
     
     app.directive('booksPanel', directives.BooksPanel);
 })();
---
+++ app.main/service/service.js
@@ -0,0 +1,5 @@
+﻿(function () {
+    var service = angular.module('apiServiceModule', ['ngResource']);
+
+
+})();
