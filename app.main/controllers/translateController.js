8927e52ff031e392041900f5b9c802e8ef692538
Berat Bilgin <berat@bilgin.com>
2014-08-03 01:55:14 +0300

    #

---
+++ app.main/controllers/translateController.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('app', ['apiServiceModule']);
+
+app.controller("AppCtrl", function ($scope, $location, _translators, _chapters) {
+    var translatorId = $location.search().translator;
+
+    var trans = _translators.get({}, { 'Id': translatorId });
+    var chaps = _chapters.query();
+
+    $scope.model = {
+        translator: trans,
+        chapters: chaps
+    }
+});
+
+
+//app.controller("AppCtrl", function ($scope, $location, _translators) {
+//    var translatorId = $location.search().translator;
+
+//    var trans = _translators.query({}, { 'Id': translatorId });
+
+//    $scope.model = {
+//        translator: trans.Name
+//    }
+//});
